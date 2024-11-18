e6b6c6631a503528c1cf345ab1164b7bf48fdc59
Adam Gombos - Desktop <gomboad@cvut.cz>
2024-11-18 12:56:53 +0100

    login frontend 1

--- login.php
+++ login.php
@@ -1,26 +1,44 @@
+<?php
+$isSignedIn = false;
+?>
+
 <!DOCTYPE html>
 <html lang="cs">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Registrační Formulář</title>
+    <link rel="stylesheet" href="styles/universal.css">
+    <link rel="stylesheet" href="styles/login.css">
+    <title>Sign up</title>
 </head>
 <body>
-    <h1>Registrační Formulář</h1>
-    <form id="registration-form">
-        <label for="username">Uživatelské jméno:</label>
-        <input type="text" id="username" name="username" placeholder="Zadejte své uživatelské jméno">
-        
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" placeholder="Zadejte svůj email">
-        
-        <label for="password">Heslo:</label>
-        <input type="password" id="password" name="password" placeholder="Zadejte své heslo">
-        
-        <span class="error">Chyba: Vyplňte toto pole správně.</span>
-        
-        <button type="submit" class="submit-btn">Odeslat</button>
-    </form>
+    <nav class="nav">
+        <a href="index.php" class="nav-title"><h1>R i m P r i n t s</h1></a>
+        <div class="nav-links">
+            <a href="">Library</a>
+            <?php if ($isSignedIn) { ?>
+                <a href="">Account</a>
+            <?php } else { ?>
+                <a href="login.php">Log in</a>
+            <?php } ?>
+        </div>
+    </nav>
+    <div class="content">
+        <h3>Sign up to RimPrints</h3>
+        <form id="registration-form">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username">
+            
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email">
+            
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password">
+            
+            <span class="error">Error: Something went wrong.</span>
+            
+            <button type="submit" class="btn-sm">Odeslat</button>
+        </form>
+    </div>
 </body>
 </html>
