a16558ef0dcd3aa646578a0c761972cc8b5b0046
Adam Gombos - Desktop <adamgombos5@gmail.com>
2024-12-03 00:19:20 +0100

    teacher notes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
-modalOpen = document.getElementById('modal-open');
-modalClose = document.getElementById('modal-close');
-modal = document.getElementById('modal');
+document.addEventListener('DOMContentLoaded', function() {
+    class Modal {
+        constructor(openButtonId, closeButtonId, modalId) {
+            this.modalOpen = document.getElementById(openButtonId);
+            this.modalClose = document.getElementById(closeButtonId);
+            this.modal = document.getElementById(modalId);
 
-modalOpen.addEventListener('click', function() {
-    modal.style.display = 'flex';
-});
+            this.addEventListeners();
+        }
 
-modalClose.addEventListener('click', function() {
-    modal.style.display = 'none';
+        addEventListeners() {
+            this.modalOpen.addEventListener('click', () => this.openModal());
+            this.modalClose.addEventListener('click', () => this.closeModal());
+        }
+
+        openModal() {
+            this.modal.style.display = 'flex';
+        }
+
+        closeModal() {
+            this.modal.style.display = 'none';
+        }
+    }
+
+    const modal = new Modal('modal-open', 'modal-close', 'modal');
 });
