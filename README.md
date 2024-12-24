53de8d0d70c50c11c83c95896612582dca6546b4
ElAdamTheGreat <adamgombos5@gmail.com>
2024-12-24 03:08:18 +0100

    Moooore queries, ToDo update

--- README.md
+++ README.md
@@ -7,10 +7,16 @@ An official repository of my semester project.
 ### Proč frontend vypadá tak otřesně? 
 Dobrá otázka. Je to z důvodu, že vytvářím stránku pro hráče jedné určiité hry, proto jsem se rozhodl, že většinu komponentů na stránce nastyluju stejně jako to již udělala tato hra. Díky tomu by se uživatelé na platformě měli cítit lépe, jelikož jsou uvítání UI, který již z většiny znají. Na neštěstí pro mě je tato hra z roku 2013, což efektivně znamená, že web bude vypadat jako z pravěku. To se snažím vybalancovat kombinací komponentů ze hry s moderním designem (například blur na navu, nebo hover u printů).  Jestli to výsledně pomůže ale jistě nevím :)
 
-# ToDo
-- frontend - all pages
-- filters - index
+# ToDo MVP
 - ~~modals~~
-- responsivity
-- user login - login (all pages - nav)
+- Print display by id in URL
+- Library
+- User login + signup
+    - $_SESSION login
+- User profile
+- frontend - all pages
 - db + backend 
+- responsivity
+## ToDo Extra
+- filters - index
+
--- server/queries.php
+++ server/queries.php
@@ -13,14 +13,9 @@ $pdo = new PDO($dsn, $user, $password);
 // Set error mode to exception
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-function getTableNames() {
-    global $pdo;
-    $query = $pdo->query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';");
-    $tables = $query->fetchAll(PDO::FETCH_ASSOC);
-    return $tables;
-}
 
-function getAll() {
+// get queries (read)
+function getAll(): array {
     global $pdo;
     $query = $pdo->query('SELECT p.*, u.id as user_id, u.username as user_username FROM "rimprints_Print" p JOIN "rimprints_User" u ON p."userId" = u.id;');
     $results = $query->fetchAll(PDO::FETCH_ASSOC);
@@ -33,8 +28,7 @@ function getAll() {
     return $prints;
 }
 
-
-function getPrintById($id) {
+function getPrintById($id): PrintModel|null {
     global $pdo;
     $query = $pdo->prepare('SELECT p.*, u.id as user_id, u.username as user_username FROM "rimprints_Print" p JOIN "rimprints_User" u ON p."userId" = u.id WHERE p.id = :id;');
     $query->execute(['id' => $id]);
@@ -47,9 +41,65 @@ function getPrintById($id) {
     return null;
 }
 
-function createPrint($title, $desc, $img, $content, $user_id) {
+function getPrintsByUserId($userId): array {
+    global $pdo;
+    $query = $pdo->prepare('SELECT p.*, u.id as user_id, u.username as user_username FROM "rimprints_Print" p JOIN "rimprints_User" u ON p."userId" = u.id WHERE p."userId" = :userId;');
+    $query->execute(['userId' => $userId]);
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    $prints = [];
+    foreach ($results as $row) {
+        $user = new UserModel($row['user_id'], $row['user_username']);
+        $prints[] = new PrintModel($row['id'], $row['title'], $row['desc'], $row['img'], $row['content'], $row['createdAt'], $row['updatedAt'], $user);
+    }
+    return $prints;
+}
+
+// Create, update, delete queries
+function createPrint($title, $desc, $img, $content, $user_id): void {
     global $pdo;
     $query = $pdo->prepare("INSERT INTO rimprints_Print (title, desc, img, content, user_id) VALUES (:title, :desc, :img, :content, :user_id);");
     $query->execute(['title' => $title, 'desc' => $desc, 'img' => $img, 'content' => $content, 'user_id' => $user_id]);
+}
+
+function updatePrint($id, $title, $desc, $img, $content): void {
+    global $pdo;
+    $query = $pdo->prepare("UPDATE rimprints_Print SET title = :title, desc = :desc, img = :img, content = :content WHERE id = :id;");
+    $query->execute(['id' => $id, 'title' => $title, 'desc' => $desc, 'img' => $img, 'content' => $content]);
+}
+
+function deletePrint($id): void {
+    global $pdo;
+    $query = $pdo->prepare("DELETE FROM rimprints_Print WHERE id = :id;");
+    $query->execute(['id' => $id]);
+}
+
+
+// User queries
+function createUser($username, $password): void {
+    global $pdo;
+    $query = $pdo->prepare("INSERT INTO rimprints_User (username, password) VALUES (:username, :password);");
+    $query->execute(['username' => $username, 'password' => $password]);
+}
+
+function loginUser($username, $password): bool {
+    global $pdo;
+    $query = $pdo->prepare('SELECT * FROM rimprints_User WHERE username = :username AND password = :password;');
+    $query->execute(['username' => $username, 'password' => $password]);
+    $row = $query->fetch(PDO::FETCH_ASSOC);
+
+    return $row !== false;
+}
+
+function getUserByUsername($username): UserModel|null {
+    global $pdo;
+    $query = $pdo->prepare('SELECT * FROM rimprints_User WHERE username = :username;');
+    $query->execute(['username' => $username]);
+    $row = $query->fetch(PDO::FETCH_ASSOC);
+
+    if ($row) {
+        return new UserModel($row['id'], $row['username']);
+    }
+    return null;
 }
 ?>
